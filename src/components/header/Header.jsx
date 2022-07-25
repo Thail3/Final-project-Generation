fcb83f5a08be95317f48b2230f79ead00d7fb510
IAMME <tanai.saen@gmail.com>
2022-07-25 20:59:06 +0700

    header-component, this is prototype header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
 
 function Header() {
-  return <div>Header</div>;
+  const options = ["bike", "car", "bus", "train", "truck", "van", "other"];
+
+  const defaultOption = options[0];
+  return (
+    <section className="header">
+      <div className="header-activity">
+        <button>ALL</button>
+        <button>RUN</button>
+        <button>SWIM</button>
+        <div className="header-dropdown">
+          <Dropdown
+            options={options}
+            value={defaultOption}
+            placeholder="Select an option"
+          />
+        </div>
+      </div>
+      <div className="header-add">
+        <button>ADD ACTIVITIES</button>
+      </div>
+    </section>
+  );
 }
 
 export default Header;
