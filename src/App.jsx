eb18b23cbe7a41bfe844126de8b569f5301b94ea
IAMME <tanai.saen@gmail.com>
2022-07-26 13:50:32 +0700

    add onClick to button

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/form/:id" element={<Form />} />
+        <Route path="/form" element={<Form />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,18 @@ import React, { useState } from "react";
 import "./header.css";
 import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
   const options = ["bike", "car", "bus", "train", "truck", "van", "other"];
 
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/form");
+  };
+
   const defaultOption = options[0];
+
   return (
     <section className="header">
       <div className="header-activity">
@@ -22,7 +29,7 @@ function Header() {
         </div>
       </div>
       <div className="header-add">
-        <button>ADD ACTIVITIES</button>
+        <button onClick={handleClick}>ADD ACTIVITIES</button>
       </div>
     </section>
   );
