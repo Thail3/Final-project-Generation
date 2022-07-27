54a3635fb18f0610fd57a98dd4e47863505d000a
IAMME <tanai.saen@gmail.com>
2022-07-27 13:29:39 +0700

    add useLocation to header compoenent

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,43 @@
 import React, { useState } from "react";
 import "./header.css";
-import Dropdown from "react-dropdown";
-import "react-dropdown/style.css";
-import { useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate("/form");
-  };
+  const location = useLocation();
+  console.log(location);
 
   return (
     <section className="header">
-      <div className="header-activity">
-        <button>ALL</button>
-        <button>RUN</button>
-        <button>SWIM</button>
-        <div className="header-dropdown">
-          <select>
-            <option value="bike">bike</option>
-            <option value="car">car</option>
-            <option value="bus">bus</option>
-            <option value="train">train</option>
-          </select>
-        </div>
-      </div>
-      <div className="header-add">
-        <button onClick={handleClick}>ADD ACTIVITIES</button>
-      </div>
+      {location.pathname === "/" ? (
+        <>
+          <div className="header-activity">
+            <button>ALL</button>
+            <button>RUN</button>
+            <button>SWIM</button>
+            <div className="header-dropdown">
+              <select>
+                <option value="bike">bike</option>
+                <option value="car">car</option>
+                <option value="bus">bus</option>
+                <option value="train">train</option>
+              </select>
+            </div>
+          </div>
+          <div className="header-add">
+            <Link to="/form">
+              <button>ADD ACTIVITIES</button>
+            </Link>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="header-activity">
+            <Link to="/">
+              <button>Back</button>
+            </Link>
+          </div>
+        </>
+      )}
     </section>
   );
 }
