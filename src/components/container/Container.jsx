17139a183c765688bf80f742c7db29541cf72d77
hambalee <hambalee@outlook.com>
2022-07-27 15:00:18 +0700

    Update container component

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import "./container.css";
+import CardComponent from "../CardComponent/CardComponent"
+import { Container as BContainer } from 'react-bootstrap';
 
-function Container() {
-  return <div>Container</div>;
+function Container(props) {
+  const renderCard = () => {
+    return (
+      <div>
+        <CardComponent />
+      </div>
+    )
+  }
+  const activities =[{}, {}, {}, {},  {}, {}, ]
+  const cards = activities.map( () => (
+    <div>
+      <CardComponent />
+    </div>
+  ))
+
+  return (
+
+    <BContainer>
+      <div class="d-flex justify-content-center align-items-center flex-wrap">
+        <div class="d-flex justify-content-center align-items-center flex-wrap">
+            {cards}
+        </div>
+      </div>
+    </BContainer>
+
+
+  )
 }
 
 export default Container;
