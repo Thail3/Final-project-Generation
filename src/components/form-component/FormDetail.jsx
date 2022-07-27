b40cbf245eb5e757f01f6efb81c1cdb1198c159c
IAMME <tanai.saen@gmail.com>
2022-07-27 16:25:19 +0700

    create formDetail component

--- src/components/form-component/FormDetail.jsx
+++ src/components/form-component/FormDetail.jsx
@@ -2,7 +2,49 @@ import React from "react";
 import "./formDetail.css";
 
 function FormDetail() {
-  return <div>Form</div>;
+  return (
+    <section>
+      <form className="form-component">
+        <div className="form-title">
+          <label htmlFor="name">Title</label>
+          <input type="text" name="title" />
+        </div>
+
+        <div className="form-img">
+          <img
+            src="https://res.cloudinary.com/dk7xxtqnj/image/upload/v1645509448/paj9bphpazesgwmj7dyc.jpg"
+            alt=""
+          />
+        </div>
+
+        <div className="form-type-select">
+          <p>Type</p>
+          <select name="type">
+            <option value="run">Run</option>
+            <option value="swim">Swim</option>
+            <option value="fly">Fly</option>
+          </select>
+        </div>
+
+        <div className="form-date-time">
+          <p>Date / Time</p>
+          <input type="date" name="date" />
+        </div>
+
+        <div className="form-duration">
+          <p>Duration</p>
+          <input type="time" name="duration" />
+        </div>
+
+        <div className="form-desc">
+          <p>Description</p>
+          <input type="text" name="description" />
+        </div>
+
+        <button>ADD ACTIVITIES</button>
+      </form>
+    </section>
+  );
 }
 
 export default FormDetail;
